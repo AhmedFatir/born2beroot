af7b028b95e35603564c95e6354d5196d791f3cf
Ahmed Fatir <123210398+AhmedFatir@users.noreply.github.com>
2023-09-14 08:47:19 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | awk '{printf("%.1f%%"), $1 + $3}')
 lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
 lvmt=$(lsblk | grep "lvm" | wc -l)
 lvmu=$(if [ $lvmt -eq 0 ]; then echo no; else echo yes; fi)
-ctcp=$(netstat -atu | grep "ESTABLISHED" | wc -l)
+ctcp=$(netstat -at | grep "ESTABLISHED" | wc -l)
 ulog=$(who | wc -l)
 ip=$(hostname -I)
 mac=$(ip a| grep ether | awk '{print $2}')
@@ -36,83 +36,78 @@ $ sudo cat /usr/local/bin/monitoring.sh
 "
 
 
-sudo tail -f /var/log/syslog
+#sudo tail -f /var/log/syslog
 
-ufw / ssh status::
-$ sudo systemctl status ufw
-$ sudo systemctl status ssh
+#ufw / ssh status::
+# $ sudo systemctl status ufw
+# $ sudo systemctl status ssh
 
-checking operating system:
-$ uname -a
+# checking operating system:
+# $ uname -a
 
-checking a username with your login:
-$ sudo cat /etc/passwd | grep <username>
+# Checking a username with your login:
+# $ sudo cat /etc/passwd | grep <username>
 
-checking you are in a group:
-$ getent group <name-group>
+# checking a group:
+# $ getent group <name-group>
 
-create new user:
-$ sudo adduser <username>
-delete user 
-$ sudo userdel <username>
+# Create new user:
+# $ sudo adduser <username>
+# delete user 
+# $ sudo userdel <username>
 
-password policy file:
-sudo vim /etc/pam.d/common-password
-/etc/security/pwquality.conf
-$ sudo crontab -l
-
-
-password checking:
-$ chage -l <username>
-
-creating group:
-$ sudo addgroup <group-name>
-delete group 
-$ sudo groupdel <group-name>
-
-assign the username to a group:
-$ sudo adduser <username> <groupname>
-
-check hostname login42:
-$ hostname
-
-modify hostname:
-$ sudo vim /etc/hostname
-sudo hostnamectl set-hostname ....
-$ sudo reboot
-
-view patitions:
-$ lsblk
-
-check that sudo/ssh/ufw is installed:
-$ dpkg -l | grep <package-name>
-
-sudo history:
-$ sudo cat /var/log/sudo/sudo.log
-sudo cat /etc/sudoers
+# password policy file:
+# sudo vim /etc/pam.d/common-password
+# /etc/security/pwquality.conf
+# $ sudo crontab -l
 
 
-allowing  ou creat port:
-$ sudo ufw allow 8080
-$ sudo vim /etc/ssh/sshd_config
-$ sudo service ssh restart
+# password checking:
+# $ chage -l <username>
 
-checking port:
-$ sudo ufw status
+# password changing dates:
+# $ chage <username>
+
+# creating group:
+# $ sudo addgroup <group-name>
+# delete group 
+# $ sudo groupdel <group-name>
+
+# assign the username to a group:
+# $ sudo adduser <username> <groupname>
+
+# check hostname login42:
+# $ hostname
+
+# modify hostname:
+# $ sudo vim /etc/hostname
+# sudo hostnamectl set-hostname ....
+# $ sudo reboot
+
+# view partitions:
+# $ lsblk
+
+# check that sudo/ssh/ufw is installed:
+# $ dpkg -l | grep <package-name>
+
+# sudo history:
+# $ sudo cat /var/log/sudo/sudo.log
+# sudo cat /etc/sudoers
 
 
-delete a port:
+# allowing or creating port:
+# $ sudo ufw allow 8080
+# $ sudo vim /etc/ssh/sshd_config
+# $ sudo service ssh restart
 
-$ sudo ufw deny 8080
-sudo ufw status numbered 
-sudo ufw delete <numbre>
+# checking port:
+# $ sudo ufw status
 
 
-log with newly created user with ssh:
-$ ssh <username>@<ip-add> -p 4242
+# Delete a port:
 
-  -l, --list                    show account aging information
-  -m, --mindays MIN_DAYS        set minimum number of days before password
-                                change to MIN_DAYS
-  -M, --maxdays MAX_DAYS        set maximum number of days before password
-                                change to MAX_DAYS
+# $ sudo ufw allow delete 8080
+
+
+# Log with newly created user with SSH:
+# $ ssh <username>@<ip-add> -p 4242
